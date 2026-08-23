ec2a9d1b74fa2ab53d879906f8dd032b0cf531c1
Industry Army Marketing <frameinsteel@gmail.com>
2026-08-23 16:50:01 -0700

    Route dedicated room visualizer page

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import Enhancements from './Enhancements.tsx';
+import VisualizerPage from './VisualizerPage.tsx';
 import {RegionalInstallerPage,regionalCities} from './RegionalPages.tsx';
 import './index.css';
 
 function Root(){
+ if(/^\/visualizer\/?$/.test(window.location.pathname)) return <VisualizerPage/>;
  const match=window.location.pathname.match(/^\/installers\/([^/]+)\/?$/);
  const slug=match?.[1];
  if(slug && regionalCities.some(city=>city[0]===slug)) return <RegionalInstallerPage slug={slug}/>;
